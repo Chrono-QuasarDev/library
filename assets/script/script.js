984dbd5bbd62f1cd86efa411f28478df253cdb60
QuasarDev <quasardev9@gmail.com>
2025-11-08 23:12:14 +0000

    feat: add function to create and store new Book instances

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,3 +7,12 @@ function Book(title, author, pages, readStatus) {
   this.readStatus = readStatus;
   this.id = crypto.randomUUID();
 }
+
+function addBookToLibrary(title, author, pages) {
+  const newBook = new Book(title, author, pages);
+  myLibrary.push(newBook);
+  return newBook
+}
+
+addBookToLibrary('The Greatest Estate Developer', 'Bk_Moon/ Lee hyunmin', 213);
+console.log(myLibrary);
