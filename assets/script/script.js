340b2bc713cba6e32d910de9263e0ad2cf86b698
QuasarDev <quasardev9@gmail.com>
2025-11-09 22:49:58 +0000

    feat: implement function to display books on page

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const displayBook = document.getElementById('library');
 
 function Book(title, author, pages, readStatus) {
   this.title = title;
@@ -8,11 +9,43 @@ function Book(title, author, pages, readStatus) {
   this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(title, author, pages) {
-  const newBook = new Book(title, author, pages);
+function addBookToLibrary(title, author, pages, readStatus) {
+  const newBook = new Book(title, author, pages, readStatus);
   myLibrary.push(newBook);
   return newBook
 }
 
-addBookToLibrary('The Greatest Estate Developer', 'Bk_Moon/ Lee hyunmin', 213);
-console.log(myLibrary);
+addBookToLibrary('The Greatest Estate Developer', 'Bk_Moon/ Lee hyunmin', 213, true);
+addBookToLibrary("Ominiscient Reader's Viewpoint", "Sing-Shong", 287, false);
+addBookToLibrary("Eternally Regressing Knight", "Lee Hyunmin", 82 , true);
+addBookToLibrary("The Knight King Who Returned with a God", "Salam Sallyeo", 136, false);
+addBookToLibrary("Boundless Necromancer", "Seonwoon", 161, true);
+
+
+function renderBook() {
+  displayBook.innerHTML = '';
+
+  myLibrary.forEach(book => {
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+
+    const title = document.createElement('h3');
+    title.textContent = book.title;
+
+    const author = document.createElement('p');
+    author.textContent = `Book author: ${book.author}`;
+
+    const pages = document.createElement('p');
+    pages.textContent = `Pages: ${book.pages}`;
+
+    const readStatus = document.createElement('p');
+
+    readStatus.textContent = book.readStatus ? "Read" : "Not Read";
+    readStatus.classList.add(book.readStatus ? 'read-status-green' : 'read-status-red');
+
+    bookCard.append(title, author, pages, readStatus);
+    displayBook.appendChild(bookCard);
+  });
+}
+
+renderBook();
